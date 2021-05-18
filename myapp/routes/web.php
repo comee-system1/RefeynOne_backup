52763d890cdd950e0690779a135d8b98929916c9
comee-system <system@comee.jp>
2021-05-18 19:00:23 +0900

    ログイン画面

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//ログインしている時のみ入れる
+Route::prefix('lss-admin')->namespace('admin')->as('admin.')->middleware('auth')->group(function() {
+    Route::get('/', [\App\Http\Controllers\Admin\indexController::class, 'index'])->name('admin.login');
+
+});
